081199238bf17124e6de5ab19b01b4149a08dfbf
Marc Bassil <marc.bassil1@gmail.com>
2015-11-18 10:46:36 -0500

    updating unit tests

--- src/testMain.cpp
+++ src/testMain.cpp
@@ -5,55 +5,48 @@
 int main(int argc, char** argv)
 {
     std::string hashedKey = simpleSHA256("marcbassil");
-    aesEncryptor e(hashedKey);
 
     {
-        while(1)
-        {
-            std::cout << "-" << std::endl;
-            std::string plain("Marc bassil");
-            std::string cipher = e.gcmEncryptString(plain);
-            std::string decrypted = e.gcmDecryptString(cipher);
+        aesEncryptor e(hashedKey);
+        std::string plain("Marc");
+        std::string cipher = e.gcmEncryptString(plain);
+        std::string decrypted = e.gcmDecryptString(cipher);
 
-                assert(plain == decrypted);
-                return 0;
-        }
+        assert(plain == decrypted);
     }
     {
-        std::cout << "-" << std::endl;
-        std::string plain("Marc bassiloiugf yoeiurhyiodusufhky4roiulkfjhksjfhksyufhhewro8347y5riweuhrf123");
+        aesEncryptor e(hashedKey);
+        std::string plain("M");
         std::string cipher = e.gcmEncryptString(plain);
         std::string decrypted = e.gcmDecryptString(cipher);
 
-        if(plain != decrypted)
-        {
-            std::cout << decrypted << std::endl;
-            assert(plain == decrypted);
-        }
+        assert(plain == decrypted);
     }
     {
-        std::cout << "-" << std::endl;
-        std::string plain("Masflidslkfshy9pw8eurrrrd498r45duw4pruweofypiewuyfrdp9wa7eynro2ui49yrseic bassil 123");
+        aesEncryptor e(hashedKey);
+        std::string plain("Mfiudhfyo97yrofnidsuhfo983nuofiusf\n\n\n\fdsjghsdkfhgkf78sdfkjhwgekf8w7egfuhd\n\n\0sdkfjgsdfjgsdkfu\0\0\0\0");
         std::string cipher = e.gcmEncryptString(plain);
         std::string decrypted = e.gcmDecryptString(cipher);
 
-        if(plain != decrypted)
-        {
-            std::cout << decrypted << std::endl;
-            assert(plain == decrypted);
-        }
+        assert(plain == decrypted);
+    }     
+    {
+        aesEncryptor e(hashedKey);
+        std::string plain("Marc 2346824   dj");
+        std::string cipher = e.gcmEncryptString(plain);
+        std::string decrypted = e.gcmDecryptString(cipher);
+
+        assert(plain == decrypted);
     } 
     {
-        std::cout << "-" << std::endl;
-        std::string plain("Marc baldkshfslidufhyosiyrf948ruoieqwuyr08weyrfoiausdhflkashdfloawuyhrliaewury3al89yuerhwlkjefhlisdufhsfsdfssil 123");
-        std::string cipher = e.gcmEncryptString(plain);
-        std::string decrypted = e.gcmDecryptString(cipher);
-
-        if(plain != decrypted)
-        {
-            std::cout << decrypted << std::endl;
-            assert(plain == decrypted);
-        }
-    } 
+        aesEncryptor e(hashedKey);
+        std::string cipherFile = "/tmp/test_file";
+        std::string plain = "testkl jdfh ksjdfh\0\0\0\0\0\0\0\0\0\n\n\n\n\n\ndfgdfgdfg\0\0\0\0dsfsdf\n\n\n\n";
+        int ret = e.gcmEncryptStringtoFile(plain, cipherFile);
+        assert(ret == 0);
+        std::string decrypted;
+        ret = e.gcmDecryptFiletoString(cipherFile, decrypted);
+        assert(plain == decrypted);
+    }
     return 0;
 }
